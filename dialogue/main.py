efc8d3d5afc70984dabc3062a245c766eeebe55f
Collas Pierre <collaspierre51@gmail.com>
2024-05-04 19:49:28 +0200

    Init api repo

---
+++ dialogue/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8001 , reload=True)
